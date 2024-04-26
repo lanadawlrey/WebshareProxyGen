ff835463b73617efb6b18d60e582fce52dac334d
ben <63555061+lanadawlrey@users.noreply.github.com>
2024-04-26 18:28:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from src.console import Console
-
 import httpx
 import random
 import time
 import threading
 import json
 
-lol = json.loads(open("config.json", 'r').read())
-
 class Webshare:
     def __init__(self) -> None:
-        self.client = httpx.Client(proxies=None, timeout=120)
-    
+        self.config = json.loads(open("config.json", 'r').read())
+        self.proxies = []
+
     def solver(self):
-       jsosn =  {
-            "clientKey": lol["key"],
+        jsosn =  {
+            "clientKey": self.config["key"],
             "task": {
                 "type": "ReCaptchaV2TaskProxyLess",
                 "websiteURL": "https://proxy2.webshare.io/register",
@@ -22,25 +20,44 @@ class Webshare:
                 "isInvisible": True,
             }
         }
-       taskid = httpx.post("https://api.capsolver.com/createTask", json=jsosn).json()["taskId"]   
-       while True:
-           ok = {"clientKey": lol["key"], "taskId": taskid}
-           response = httpx.post("https://api.capsolver.com/getTaskResult", json=ok)
-           #print(response.text)
-           if response.json()["status"] == "ready":
+        taskid = httpx.post("https://api.capsolver.com/createTask", json=jsosn).json()["taskId"]   
+        while True:
+            ok = {"clientKey": self.config["key"], "taskId": taskid}
+            response = httpx.post("https://api.capsolver.com/getTaskResult", json=ok)
+            if response.json()["status"] == "ready":
                 return response.json()["solution"]["gRecaptchaResponse"]
-           else:
-               # print(response.text)
-               pass
-           
-           time.sleep(0.5)
-
-       
-
+            else:
+                pass
+            time.sleep(0.5)
 
     def register(self):
         while True:
             try:
+                if not self.proxies:
+                    with open("output/proxies.txt", "r") as f:
+                        self.proxies = f.read().splitlines()
+
+                proxy = random.choice(self.proxies)
+                proxy_parts = proxy.split('@')
+                proxy_userpass = proxy_parts[0]
+                proxy_ipport = proxy_parts[1]
+
+                if ':' in proxy_userpass:
+                    proxy_user, proxy_pass = proxy_userpass.split(':')
+                else:
+                    proxy_user = proxy_userpass
+                    proxy_pass = None
+
+                if ':' in proxy_ipport:
+                    proxy_ip, proxy_port = proxy_ipport.split(':')
+                else:
+                    raise ValueError("Invalid proxy format")
+
+                proxy_dict = {
+                    "http://": f"http://{proxy_user}:{proxy_pass}@{proxy_ip}:{proxy_port}",
+                    "https://": f"http://{proxy_user}:{proxy_pass}@{proxy_ip}:{proxy_port}"
+                }
+
                 headers = {
                     'authority': 'proxy.webshare.io',
                     'accept': 'application/json, text/plain, */*',
@@ -56,8 +73,10 @@ class Webshare:
                     'sec-fetch-site': 'same-site',
                     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
                 }
+
                 key = self.solver()
                 Console.log(f"Solved {key[:23]}")
+
                 self.domains = ["@gmail.com", "@yahoo.com", "@outlook.com", "@hotmail.com", "@protonmail.com"]
                 self.email =  f"Slotths" + "_" + "".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=8)) + random.choice(self.domains)
 
@@ -67,45 +86,34 @@ class Webshare:
                     'tos_accepted': True,
                     'recaptcha': key
                 }
-                response = self.client.post('https://proxy.webshare.io/api/v2/register/', headers=headers, json=json_data)
-                if response.status_code == 200:
-                    token = response.json()["token"]
-                    Console.success(f"Made Account: {self.email}:lkjsdflkjsldkfj")
 
-                    headers = {
-                        'authority': 'proxy.webshare.io',
-                        'accept': 'application/json, text/plain, */*',
-                        'accept-language': 'en-US,en;q=0.9',
-                        'authorization': f'Token {token}',
-                        'origin': 'https://proxy2.webshare.io',
-                        'referer': 'https://proxy2.webshare.io/',
-                        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-                        'sec-ch-ua-mobile': '?0',
-                        'sec-ch-ua-platform': '"macOS"',
-                        'sec-fetch-dest': 'empty',
-                        'sec-fetch-mode': 'cors',
-                        'sec-fetch-site': 'same-site',
-                        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-                    }
+                with httpx.Client(proxies=proxy_dict) as session:
+                    response = session.post('https://proxy.webshare.io/api/v2/register/', headers=headers, json=json_data)
+                    if response.status_code == 200:
+                        token = response.json()["token"]
+                        Console.success(f"Made Account: {self.email}:lkjsdflkjsldkfj")
 
-                    params = {
-                        'mode': 'direct',
-                        'page': '1',
-                        'page_size': '10',
-                    }
+                        headers['authorization'] = f'Token {token}'
 
-                    response = self.client.get('https://proxy.webshare.io/api/v2/proxy/list/', params=params, headers=headers)
-                    for i in response.json()["results"]:
-                        username = i["username"]
-                        password = i["password"]
-                        ip = i["proxy_address"]
-                        port = i["port"]
-                        Console.success(f"Created {username}:{password}@{ip}:{port}")
-                        with open("output/proxies.txt", "a") as e:
-                            e.write(f"{username}:{password}@{ip}:{port}\n")
-                else:
-                    print(response.text)
-            except:
+                        params = {
+                            'mode': 'direct',
+                            'page': '1',
+                            'page_size': '10',
+                        }
+
+                        response = session.get('https://proxy.webshare.io/api/v2/proxy/list/', params=params, headers=headers)
+                        for i in response.json()["results"]:
+                            username = i["username"]
+                            password = i["password"]
+                            ip = i["proxy_address"]
+                            port = i["port"]
+                            Console.success(f"Created {username}:{password}@{ip}:{port}")
+                            with open("output/proxies.txt", "a") as e:
+                                e.write(f"{username}:{password}@{ip}:{port}\n")
+                    else:
+                        print(response.text)
+            except Exception as e:
+                print(e)
                 pass
 
 for i in range(10):
